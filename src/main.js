0d8a60ed62d19423a503104d3003b95252e101b4
M-Stable <fchanghlee@gmail.com>
2021-01-06 23:37:43 +1300

    update firebase config

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import firebase from "firebase/app";
-import VueMq from 'vue-mq'
+import VueMq from "vue-mq";
 
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
@@ -14,8 +14,8 @@ Vue.use(VueMq, {
     tablet: 900,
     laptop: 1250,
     desktop: Infinity,
-  }
-})
+  },
+});
 
 const firebaseConfig = {
   apiKey: "AIzaSyAu36cu6UWFg7l-4XBnfgj3cOQ-ydeTRis",
@@ -25,6 +25,15 @@ const firebaseConfig = {
   storageBucket: "what-to-eat-fe48a.appspot.com",
   messagingSenderId: "199142113995",
   appId: "1:199142113995:web:d2c58faa700ea30be0914e",
+  hosting: {
+    public: "dist",
+    rewrites: [
+      {
+        source: "**",
+        destination: "/index.html",
+      },
+    ],
+  },
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
